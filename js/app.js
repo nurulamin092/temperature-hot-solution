3fa55505ae2e5d135424476b6c072271d0b520ec
nurulamin092 <mnurulamin092@gmial.com>
2022-03-02 12:18:43 +0600

    add daynamic city name and wather

--- js/app.js
+++ js/app.js
@@ -4,9 +4,20 @@ const searchTemperature = () => {
     const city = document.getElementById('city-name').value;
     // const url = `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon=${city}&appid=${API_KEY}`;
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
     fetch(url)
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => displayTemperature(data))
 
+}
+
+const setInnerText = (id, text) => {
+    document.getElementById(id).innerText = text;
+}
+const displayTemperature = temperature => {
+    setInnerText('city', temperature.name);
+    setInnerText('temperature', temperature.main.temp);
+    setInnerText('condition', temperature.weather[0].main);
+    setInnerText('icon', temperature.weather[0].icon)
+
+    console.log(temperature);
 }
